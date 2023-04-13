2f639cbab1b5bc1e635832636212c85a308b5918
dmitryger <you@example.com>
2023-04-13 14:01:20 +0300

    fix create project

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -1,4 +1,4 @@
 def dataset_process(ticket, project_pk):
     from api.services.csv_import.import_symbols_data import download_data
 
-    download_data.delay(ticket, project_pk)
+    download_data(ticket, project_pk)
